c99b844ead2421cc90d4190fd2f93bf939ac06f4
IvanStukalov <ivanstukalov2003@gmail.com>
2023-12-27 16:00:28 +0300

    finished async service

---
+++ async/async_app/views.py
@@ -0,0 +1,37 @@
+import json
+import time
+import random
+import requests
+from concurrent import futures
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+executor = futures.ThreadPoolExecutor(max_workers=1)
+ServerToken = "rjbrbjwf4908h8nfieh"
+url = "http://127.0.0.1:8080/api/event/finish-scanning"
+
+def do_scanning(req_body):
+		scanned_percent = random.randint(1, 100)
+		time.sleep(5)
+		req_body['scanned_percent'] = scanned_percent
+		return req_body
+
+def status_callback(task):
+    try:
+      result = task.result()
+      print(result)
+    except futures._base.CancelledError:
+      return
+    requests.put(url, data=json.dumps(result), timeout=3)
+
+@api_view(['Put'])
+def scan(request):
+    req_body = json.loads(request.body)
+    if req_body["Token"] == ServerToken:
+        task = executor.submit(do_scanning, req_body)
+        task.add_done_callback(status_callback)        
+        return Response(status=status.HTTP_200_OK)
+    else:
+        return Response(status=status.HTTP_403_FORBIDDEN)
+
