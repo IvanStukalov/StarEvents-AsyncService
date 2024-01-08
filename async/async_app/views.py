3b4b29caad5094c1a770bcce486c46b85f32f09d
IvanStukalov <ivanstukalov2003@gmail.com>
2024-01-08 23:23:09 +0300

    fixed token login

--- async/async_app/views.py
+++ async/async_app/views.py
@@ -12,10 +12,11 @@ ServerToken = "rjbrbjwf4908h8nfieh"
 url = "http://127.0.0.1:8080/api/event/finish-scanning"
 
 def do_scanning(req_body):
-		scanned_percent = random.randint(1, 100)
-		time.sleep(5)
-		req_body['scanned_percent'] = scanned_percent
-		return req_body
+    scanned_percent = random.randint(1, 100)
+    time.sleep(5)
+    req_body['scanned_percent'] = scanned_percent
+    req_body['Token'] = ServerToken
+    return req_body
 
 def status_callback(task):
     try:
@@ -28,10 +29,6 @@ def status_callback(task):
 @api_view(['Put'])
 def scan(request):
     req_body = json.loads(request.body)
-    if req_body["Token"] == ServerToken:
-        task = executor.submit(do_scanning, req_body)
-        task.add_done_callback(status_callback)        
-        return Response(status=status.HTTP_200_OK)
-    else:
-        return Response(status=status.HTTP_403_FORBIDDEN)
-
+    task = executor.submit(do_scanning, req_body)
+    task.add_done_callback(status_callback)        
+    return Response(status=status.HTTP_200_OK)
